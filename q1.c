c9fb19135b4b707900abea68bf7f26759ad1465c
princexweb <user.email : "princexbundela@gmail.com>
2025-05-12 22:42:50 +0530

    add program in c

--- q1.c
+++ q1.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
-int main(){
-    int a = 12;
-    int b = 32;
-    printf("%d",a+b);
+// Q1. Take two integers a & b with some constant values. Print their sum on the console
+int main(){ 
+    int a;
+    int b;
+    printf("enter the vlaue of a ");
+    scanf("%d",&a);
+    printf("enter the vlaue of b ");
+    scanf("%d",&b);
+    printf("the sum of %d and %d is %d",a,b,a+b);
+    
     return 0;
 }
